9bd6076b2ad2387dc7f23242fe7790e5cb8d548e
Theozzzzzz <theo.strindmark@gmail.com>
2025-01-24 09:29:46 +0100

    Footer big fix

--- loadInFooter.js
+++ loadInFooter.js
@@ -4,18 +4,27 @@ document.getElementsByTagName('footer')[0].innerHTML = `
 <div class="footer-logo-container">
     <img src="https://raw.githubusercontent.com/Theozzzzzz/Gymnasiearbete/f67c7ba91a42b353e13b780c89d3be45f9b4f91e/images/NormalLogo_endast_Skalbagge.png" alt="Circus Normals logo" id="footer-image">
 </div>
-<p>copyright theozzz 2024</p>
+<div class="Samarbetspartner">
+    <h1>Samarbetspartner</h1>
+    <h2> Fler bollar i luften AB</h2>
+    <img src="/images/flerbollar.jpg" alt="Samarbetspartner">
+</div>
 <div class="socials-container">
-    <p> föj oss på sociala medier: </p>
-    <a href="https://www.instagram.com/circusnormal/" target="_blank" class="footer-sociala-medier-link">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" class="footer-sociala-medier-logo" alt="instagram">
-    </a>
-    <a href="https://www.facebook.com/circusnormal" target="_blank" class="footer-sociala-medier-link">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg" class="footer-sociala-medier-logo" alt="facebook">
-    </a>
-    <a href="https://www.youtube.com/cirkusnormal" target="_blank" class="footer-sociala-medier-link">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/f/fd/YouTube_full-color_icon_%282024%29.svg" class="footer-sociala-medier-logo" alt="youtube">
-    </a>
+    <ul>
+        <p> Föj oss på sociala medier </p> <br>
+        <a href="https://www.instagram.com/circusnormal/" target="_blank" class="footer-sociala-medier-link">
+            <i class="fa-brands fa-instagram footer-sociala-medier-logo"></i>
+        </a>
+        <a href="https://www.facebook.com/circusnormal" target="_blank" class="footer-sociala-medier-link">
+            <i class="fa-brands fa-facebook-f footer-sociala-medier-logo"></i>
+        </a>
+        <a href="https://www.youtube.com/cirkusnormal" target="_blank" class="footer-sociala-medier-link">
+            <i class="fa-brands fa-youtube footer-sociala-medier-logo"></i>
+        </a>
+    </ul>
+</div>
+<div class="copyright">
+    <p> Copyright 2024 Cirkus Normal | cirkusnormal.se | GA |</p>
 </div>
 
 `
@@ -27,22 +36,53 @@ var style = document.getElementsByTagName('style')[0];
 style.innerHTML += `
 footer {
     display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
     alaign-items: center;
+    flex-wrap: wrap;
     padding: 0;
     margin: 0;
     background-color: var(--color-tertiary);
     border-radius: 0 30px 0 0;
-    height: 70px;
+    height: 30vh;
     bottom: 0;
 }
 
+p{
+    color:white;
+}
+
+.copyright{
+    display:flex;
+    align-items: flex-end;
+    color:white;
+}
 .footer-logo-container {
     display: flex;
     justify-content: center;
     align-items: center;
+    margin-left: 40px;
     > img {
-    height: 70%;
+    height: 40%;
+    }
+}
+
+.Samarbetspartner{
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    height: 100%;
+    with: fit-content;
+    > h1 {
+        color: white;
+        font-size: 25px;
+    }
+    > h2 {
+        color: white;
+        font-size: 20px;
+    }
+    > img {
+        max-height: 90%;
+        margin: 5%;
     }
 }
 
@@ -52,7 +92,9 @@ footer {
 }
 
 .footer-sociala-medier-logo {
-    filter: grayscale(100%);
+    color: whitesmoke;
+    padding-right: 20px;
+    font-size: 40px;
     height: 100%;
     margin: 0 5px;
 }
@@ -66,5 +108,24 @@ footer {
     padding: 0;
     margin: 0;
     height: 100%;
+    > ul{
+        margin-right: 10vh;
+        > a {
+            margin-left: 2vh;
+        }
+        > p {
+        font-size: 25px;
+        color: white;
+        }
+    } 
 }
+
+@media (max-width: 480px) {
+    .footer-logo-container{
+        display:none;
+    }
+}
+
+
+    
 `
--- loadinHeader.js
+++ loadinHeader.js
@@ -138,6 +138,8 @@ html {
     box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
     z-index: 1;
     width: 11%;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
 }
 
 .dropbtn{
